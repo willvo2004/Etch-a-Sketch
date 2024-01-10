65fdf836bf9f6231a760bb9afb33545dfc31443c
willvo2004 <willvo2004@gmail.com>
2024-01-10 17:39:48 -0500

    allow user to change the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
 const container = document.querySelector(".div-container")
 
-
-for (let i = 0; i < 256 ; i++) {
+for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.setAttribute("class","grid-item");
+    square.style.width = `calc(6.25% - 10px)`;
+    square.style.height = `calc(6.25% - 10px)`;
     container.appendChild(square);
 }
 
-const hoverSqaureList = document.querySelectorAll(".grid-item");
-hoverSqaureList.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "#c8d6bf";
-    });
-});
+addHoverHandler()
 
 const slider = document.querySelector(".slider");
 const value = document.querySelector("#value");
@@ -20,5 +16,47 @@ value.innerHTML = slider.value + " x " + slider.value;
 
 slider.oninput = function() {
     value.innerHTML = this.value + " x " + this.value;
+    updateGrid();
   }
 
+  function updateGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+    const grid = slider.value;
+    for (let i = 0; i < grid**2 ; i++) {
+        const sqaure = createGridItems(grid);
+        container.appendChild(sqaure);
+    }
+}
+
+function updateGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+    const grid = slider.value;
+    for (let i = 0; i < grid**2 ; i++) {
+        const sqaure = createGridItems(grid);
+        container.appendChild(sqaure);
+    }
+    addHoverHandler()
+}
+
+function createGridItems(gridSize) {
+    const square = document.createElement("div");
+    square.setAttribute("class","grid-item");
+    square.style.width = `calc(${100/gridSize}% - 10px)`;
+    square.style.height = `calc(${100/gridSize}% - 10px)`;
+    return square;
+}
+
+function addHoverHandler() {
+    const hoverSqaureList = document.querySelectorAll(".grid-item");
+    hoverSqaureList.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "#c8d6bf";
+    });
+});
+}
