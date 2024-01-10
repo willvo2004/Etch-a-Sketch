73278c6150917c556b1e6d9cae6bd7eb03350eb3
willvo2004 <willvo2004@gmail.com>
2024-01-10 07:10:24 -0500

    added hover feature

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 const container = document.querySelector(".div-container")
-const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", () => {
-    for (let i = 0; i < 9 ; i++) {
-        const square = document.createElement("div");
-        square.setAttribute("class","grid-item");
-        container.appendChild(square);
-    }
+
+for (let i = 0; i < 256 ; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class","grid-item");
+    container.appendChild(square);
+}
+
+const hoverSqaureList = document.querySelectorAll(".grid-item");
+hoverSqaureList.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "red";
+    });
 });
+
+
